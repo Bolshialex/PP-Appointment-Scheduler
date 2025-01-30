b76c67a2b001122428ce09d77b8062e9925dff42
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-01-30 14:29:20 -0800

    Add server routes

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 import express from "express";
+import colors from "colors";
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 const PORT = 3030;
-let orders = [];
+let appointments = [];
 
 app.get("/", (req, res) => {
   res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
 
 app.post("/submit-order", (req, res) => {
-  orders.push(req.body);
+  console.log(req.body);
+  appointments.push(req.body);
+});
+
+app.get("/admin/appointments", (req, res) => {
+  res.send(appointments);
 });
 
 app.listen(PORT, () => {
