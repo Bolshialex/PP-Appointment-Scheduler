683122f48f56c10b9256d14deb6681181b962d73
Tim <timmywilliams4665@gmail.com>
2025-01-30 17:08:37 -0800

    Forgot to push

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = 3030;
 let appointments = [];
 
+
+
 app.get("/", (req, res) => {
   res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
 
 app.post("/submit-order", (req, res) => {
   appointments.push(req.body);
+  const timeStamp = Date.now();
+  console.log(timeStamp)
   res.send(
     `<h1>Thank you for scheduling an appointment ${req.body.fname}</h1>`
   );
